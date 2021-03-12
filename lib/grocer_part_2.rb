d5bf96e933c9908d9a3968d2b46c4b9c6e406980
Zecharias Mengis <zmengis@gmail.com>
2021-03-12 15:51:54 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon
-        cart_item[count] -= coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
       end
     end
     counter += 1
