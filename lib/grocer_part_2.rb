6fd674f6fb578a9e92d7d7602b7d6be3164d00d8
Zecharias Mengis <zmengis@gmail.com>
2021-03-12 16:18:15 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -41,5 +41,7 @@ def checkout(cart, coupons)
   couponed_cart = apply_coupons(consolidated_cart)
   clearanced_cart = apply_clearance(couponed_cart)
   
-  
+  clearanced_cart.each do |cart_items|
+    binding.pry
+  end
 end
