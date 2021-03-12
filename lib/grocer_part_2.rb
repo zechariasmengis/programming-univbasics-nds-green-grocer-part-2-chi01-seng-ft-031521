8a9c2b50a1dc981b69b6dc63cca26429c5bacebc
Zecharias Mengis <zmengis@gmail.com>
2021-03-12 15:46:38 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -13,7 +13,8 @@ def apply_coupons(cart, coupons)
       else
         cart_item_with_coupon = {
           :item => coupouned_item_name,
-          :price => coupons[counter][:cost] / 
+          :price => coupons[counter][:cost] / coupons[counter][:num],
+          
         }
   end
 end
