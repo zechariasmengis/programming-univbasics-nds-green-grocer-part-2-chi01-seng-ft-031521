b1bffac19f4ec7139318e60412ef986383d7a08d
Zecharias Mengis <zmengis@gmail.com>
2021-03-12 15:42:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,6 +5,7 @@ def apply_coupons(cart, coupons)
   while counter < coupons.length
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
     coupouned_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon =
   end
 end
 
